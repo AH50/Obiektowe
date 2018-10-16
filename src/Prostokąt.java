39b78b624d7bb9417c81160ac40c7936c92a5cdc
Adam <adhanderek@gmail.com>
2018-10-16 13:36:19 +0200

    a

---
+++ src/Prostokąt.java
@@ -0,0 +1,30 @@
+package obiektowe;
+public class Prostokąt extends Kwadrat {
+    protected double b_;
+
+    public Prostokąt(double a, double b)
+    {
+        super(a);
+        b_ = b;
+    }
+
+    public double getB()
+    {
+        return b_;
+    }
+
+    public void setB(double b)
+    {
+        b_ = b;
+    }
+
+    public double area()
+    {
+        return a_*b_;
+    }
+    public boolean isBigger(Prostokąt other)
+    {
+        return other.area()>area();
+    }
+
+}
