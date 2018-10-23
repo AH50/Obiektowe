28ea0f7df8feefbbcb9102acf01ab8ab411686fd
Adam <adhanderek@gmail.com>
2018-10-23 20:12:29 +0200

    test

---
+++ src/lab3/EmailMessage.java
@@ -0,0 +1,134 @@
+package lab3;
+
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.util.LinkedList;
+
+public class EmailMessage {
+    private String from;
+    private LinkedList<String> to;
+    private String subject;
+    private String content;
+    private String mimeType;
+    private LinkedList<String> cc;
+    private LinkedList<String> bcc;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public LinkedList<String> getTo() {
+        return to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public LinkedList<String> getCc() {
+        return cc;
+    }
+
+    public LinkedList<String> getBcc() {
+        return bcc;
+    }
+
+    public static Builder builder() {
+        return new EmailMessage.Builder();
+    }
+
+    protected EmailMessage(String from, LinkedList<String> to, String subject, String content, String mimeType, LinkedList<String> cc, LinkedList<String> bcc)
+    {
+        this.from = from;
+        this.to = to;
+        this.subject = subject;
+        this.content = content;
+        this.mimeType = mimeType;
+        this.cc = cc;
+        this.bcc = bcc;
+    }
+
+    public static class Builder
+    {
+        private String from;
+        private LinkedList<String> to = new LinkedList();
+        private String subject;
+        private String content;
+        private String mimeType;
+        private LinkedList<String> cc = new LinkedList();
+        private LinkedList<String> bcc = new LinkedList();
+
+        public Builder addFrom(String from_){
+            if(isValidEmail(from_)) {
+                this.from = from_;
+            }
+            else
+            {
+                System.out.printf("Błędny adres email!");
+            }
+
+            return this;
+        }
+        public Builder addTo(String to_)
+            {
+            if(isValidEmail(to_)) {
+                this.to.add(to_);
+            }
+            else
+            {
+                System.out.printf("Błędny adres email!");
+            }
+            return this;
+        }
+        public Builder addSubject(String subject_)
+        {
+            subject = subject_;
+            return this;
+        }
+        public Builder addContent(String content_)
+        {
+            content = content_;
+            return this;
+        }
+        public Builder addMimeType(String mimeType_)
+        {
+            mimeType = mimeType_;
+            return this;
+        }
+        public Builder addCC(String cc_)
+        {
+            cc.add(cc_);
+            return this;
+        }
+        public Builder addBCC(String bcc_)
+        {
+            bcc.add(bcc_);
+            return this;
+        }
+        public EmailMessage build()
+        {
+            return new EmailMessage(this.from, this.to, this.subject, this.content, this.mimeType, this.cc, this.bcc);
+        }
+        public static boolean isValidEmail(String email)
+        {
+            boolean result = true;
+            try{
+                InternetAddress mail = new InternetAddress(email);
+                mail.validate();
+            } catch (AddressException d)
+            {
+                result = false;
+            }
+            return result;
+        }
+        }
+}
--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -1,4 +1,61 @@
 package lab3;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        EmailMessage wiadomosc = EmailMessage.builder()
+                .addFrom("adspammail50@gmail.com")
+                .addTo("adspammail50@gmail.com")
+                .addSubject("Tytuł")
+                .addContent("Treść maila")
+                .build();
+        send(wiadomosc);
+    }
+    private static void send(EmailMessage message1) {
+        String username;
+        String password;
+        System.out.print("Podaj haslo:");
+        username = message1.getFrom();
+        Scanner scan = new Scanner(System.in);
+        password = scan.nextLine();
+
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        Session session = Session.getDefaultInstance(props,
+                new Authenticator()
+                {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username,password);
+                    }
+                });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(message1.getFrom()));
+            for (int i = 0; i < message1.getTo().size(); i++) {
+                message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(message1.getTo().get(i)));
+            }
+            message.setSubject(message1.getSubject());
+            message.setText(message1.getContent());
+            Transport.send(message);
+            System.out.println("Wiadomość została wysłana poprawnie");
+
+        } catch (MessagingException obj) {
+            obj.printStackTrace();
+        }
+    }
 }
