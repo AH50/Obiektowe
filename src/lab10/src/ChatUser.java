890564ce554c3283e26c1967879e2456cd0d5027
Adam Handerek <adhanderek@gmail.com>
2018-12-14 11:11:16 +0100

    a

---
+++ src/lab10/src/ChatUser.java
@@ -0,0 +1,50 @@
+
+import java.net.*;
+import java.io.*;
+
+public class ChatUser {
+    private String hostname;
+    private int port;
+    private String userName;
+
+    public ChatUser(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+
+            System.out.println("Connected to the chat server");
+
+            new ReadT(socket, this).start();
+            new WriteT(socket, this).start();
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+
+    }
+
+    void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    String getUserName() {
+        return this.userName;
+    }
+
+
+    public static void main(String[] args) {
+        if (args.length < 2) return;
+
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        ChatUser client = new ChatUser(hostname, port);
+        client.execute();
+    }
+}
